6c5f6f7ddf63e26fd6f506fda6c309c13d06d1bd
Kumaresan K <tkkumaresan@gmail.com>
2021-02-13 15:52:56 +0530

    history navigation fixed

--- app/src/main/java/com/deepan/dsuite/MainActivity.java
+++ app/src/main/java/com/deepan/dsuite/MainActivity.java
@@ -1,23 +1,38 @@
 package com.deepan.dsuite;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 
 public class MainActivity extends Activity {
     private static final String URL = "https://dsuite.webbehinds.com";
-
+    WebView myWebView;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView = (WebView) findViewById(R.id.webView);
         WebSettings settings = myWebView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         myWebView.clearSslPreferences();
         myWebView.loadUrl(URL);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 
 }
